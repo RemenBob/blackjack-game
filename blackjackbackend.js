9d59de2a6ceb3b64c7bae00d9c9faaed225a79f5
RemenBob <110488494+RemenBob@users.noreply.github.com>
2022-08-12 21:44:33 -0700

    recreated deck at the beginning of start game to allow replayability over 26 attempts

--- blackjackbackend.js
+++ blackjackbackend.js
@@ -10,7 +10,7 @@ let possibleCards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 createDeck();
 
 function startGame() {
-    
+    createDeck();
     let firstCard = deck[0];
     let secondCard = deck[1];
     sum = firstCard + secondCard;
